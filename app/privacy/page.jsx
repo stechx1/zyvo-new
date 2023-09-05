8f0a012db7b472d207fa3d5a95a93a9c2ac5a51f
techx1 <ss.uitech01@gmail.com>
2023-09-06 02:56:58 +0500

    added privacy and terms

---
+++ app/privacy/page.jsx
@@ -0,0 +1,152 @@
+'use client';
+import { poppins } from '@/utils/font';
+const PrivacyPage = () => {
+  return (
+    <div className=''>
+      <div className='container mx-auto flex flex-col my-8  justify-center items-center'>
+        <h2
+          className={`text-center font-bold text-4xl my-4 ${poppins.className}`}
+        >
+          Privacy Policy
+        </h2>
+        <div className='flex flex-col gap-6'>
+          <p>
+            At Zyvo, we are committed to protecting your privacy and ensuring
+            the security of your personal information. This Privacy Policy
+            outlines how we collect, use, and safeguard your data when you use
+            our hourly rental platform. By using Zyvo, you consent to the
+            practices described in this policy.
+          </p>
+          <div className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`1) Information We Collect:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  <span className='font-bold'>Personal Information:</span> When
+                  you sign up for Zyvo, we collect your name, email address,
+                  phone number, and payment information.
+                </li>
+                <li>
+                  <span className='font-bold'>Usage Information: </span>We may
+                  collect information about how you use our platform, including
+                  your search queries, rental history, and preferences.
+                </li>
+                <li>
+                  <span className='font-bold'>Device Information: </span>We may
+                  collect information about the device you use to access Zyvo,
+                  such as your IP address, browser type, and operating system
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`2) How We Use Your Information:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  <span className='font-bold'>Provide Services:</span> We use
+                  your personal information to facilitate and improve your
+                  experience on Zyvo, including processing bookings,
+                  communicating with you, and resolving any issues.
+                </li>
+                <li>
+                  <span className='font-bold'>Personalization: </span>We may use
+                  your information to personalize your experience, recommend
+                  relevant rentals, and send you promotional offers.
+                </li>
+                <li>
+                  <span className='font-bold'>Analytics: </span>We analyze usage
+                  patterns and trends to improve our platform, enhance user
+                  experience, and develop new features.
+                </li>
+                <li>
+                  <span className='font-bold'>Legal Compliance: </span>We may
+                  use your information to comply with applicable laws,
+                  regulations, or legal processes.
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`3) Data Sharing:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  <span className='font-bold'>Service Providers:</span> We may
+                  share your information with trusted third-party service
+                  providers who assist us in operating our platform, processing
+                  payments, and delivering services.
+                </li>
+                <li>
+                  <span className='font-bold'>Legal Requirements: </span>We may
+                  disclose your information if required by law, court order, or
+                  government request.
+                </li>
+                <li>
+                  <span className='font-bold'>Business Transfers: </span>In the
+                  event of a merger, acquisition, or sale of Zyvo, your
+                  information may be transferred to the new entity.
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`4) Data Security:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  We implement industry-standard security measures to protect
+                  your personal information from unauthorized access,
+                  disclosure, or alteration.
+                </li>
+                <li>
+                  However, no method of transmission over the internet or
+                  electronic storage is 100% secure, and we cannot guarantee
+                  absolute security.
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`5) Third-Party Links:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  Zyvo may contain links to third-party websites or services. We
+                  are not responsible for the privacy practices or content of
+                  these third parties. We encourage you to review their privacy
+                  policies before providing any personal information.
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`6) Children's Privacy:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  Zyvo is not intended for use by individuals under the age of
+                  18. We do not knowingly collect personal information from
+                  children. If we become aware of any such data, we will
+                  promptly delete it.
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`7) Changes to Privacy Policy:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  We may update this Privacy Policy from time to time. Any
+                  changes will be posted on Zyvo, and the revised policy will be
+                  effective immediately upon posting.
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <p>
+            If you have any questions or concerns about our Privacy Policy,
+            please contact us at [email address].
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PrivacyPage;
---
+++ app/terms-of-service/page.jsx
@@ -0,0 +1,144 @@
+'use client';
+import { poppins } from '@/utils/font';
+const TermsPage = () => {
+  return (
+    <div className=''>
+      <div className='container mx-auto flex flex-col my-8  justify-center items-center'>
+        <h2
+          className={`text-center font-bold text-4xl my-4 ${poppins.className}`}
+        >
+          Terms of Service for Zyvo
+        </h2>
+        <div className='flex flex-col gap-6'>
+          <p>
+            {`These Terms of Service ("Terms") govern your use of Zyvo's hourly
+            rental platform. By accessing or using Zyvo, you agree to be bound
+            by these Terms. If you do not agree with any part of these Terms,
+            you may not use Zyvo.`}
+          </p>
+          <div className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`1. User Responsibilities:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>You must be at least 18 years old to use Zyvo.</li>
+                <li>
+                  You are responsible for maintaining the confidentiality of
+                  your account information and for all activities that occur
+                  under your account.
+                </li>
+                <li>
+                  You agree to provide accurate and up-to-date information when
+                  using Zyvo.
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`2) Rental Listings and Bookings:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  Zyvo allows users to list and book hourly rentals. As a
+                  renter, you agree to pay the listed rental price and any
+                  additional fees specified by the host.
+                </li>
+                <li>
+                  As a host, you agree to accurately describe your rental and
+                  provide it in the condition specified in the listing.
+                </li>
+                <li>
+                  Zyvo is not responsible for any disputes between renters and
+                  hosts. We encourage users to communicate and resolve any
+                  issues directly.
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`3) Prohibited Activities:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  You may not use Zyvo for any illegal or unauthorized purpose.
+                </li>
+                <li>
+                  You may not violate any laws, regulations, or third-party
+                  rights.
+                </li>
+                <li>
+                  {`You may not engage in any activity that could harm Zyvo's
+                  reputation or interfere with its operation.`}
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`4) Intellectual Property:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  {`Zyvo and its content are protected by copyright, trademark,
+                  and other intellectual property laws. - You may not use,
+                  reproduce, modify, or distribute Zyvo's content without prior
+                  written permission.`}
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`5) Limitation of Liability:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  Zyvo is not responsible for any damages, losses, or
+                  liabilities arising from your use of the platform or any
+                  rental transactions.
+                </li>
+                <li>
+                  Zyvo does not guarantee the availability, quality, or safety
+                  of any rentals listed on the platform.
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`6) Indemnification:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  You agree to indemnify and hold Zyvo harmless from any claims,
+                  damages, or losses arising from your use of the platform or
+                  any violation of these Terms.
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`7) Termination:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  Zyvo may terminate or suspend your access to the platform at
+                  any time, without prior notice or liability, for any reason.
+                </li>
+              </ul>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <h2 className='font-bold text-xl'>{`8) Governing Laws:`}</h2>
+              <ul className='list-disc ml-6'>
+                <li>
+                  These Terms are governed by and construed in accordance with
+                  the laws of [Georgia]. Any disputes arising from these Terms
+                  shall be resolved in the courts of [Georgia].
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <p>
+            If you have any questions or concerns about these Terms of Service,
+            please contact us at{' '}
+            <span className='font-bold'>support@zyvoapp.com</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TermsPage;
