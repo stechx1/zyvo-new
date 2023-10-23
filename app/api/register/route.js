ca531678e32942790ed0f3527b916840e76d08ee
Tahirkhan3013 <77354419+Tahirkhan3013@users.noreply.github.com>
2023-10-23 23:38:40 +0500

    Update route.js

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,32 +1,12 @@
 import { NextResponse } from 'next/server';
-const nodemailer = require('nodemailer');
 const axios = require('axios');
-
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: process.env.NEXT_PUBLIC_EMAIL,
-    pass: process.env.NEXT_PUBLIC_EMAIL_PASSWORD,
-  },
-});
 
 export async function POST(req, res) {
   if (req.method === 'POST') {
     const body = await req.json();
     const { email, firstName } = body; // Assuming you have collected email and first name
 
-    const mailOptions = {
-      from: process.env.NEXT_PUBLIC_EMAIL,
-      to: email,
-      subject: 'Welcome to the Zyvo Universe!',
-      text: 'Thank you for signing up for our app. We are excited to have you on board!',
-      html: `<!-- Your email content here -->`,
-    };
-
     try {
-      // Send the welcome email
-      await transporter.sendMail(mailOptions);
-
       // Subscribe the user to Mailchimp
       const apiKey = '81d72bba2bda26144daf5d8f1c815739-us21'; // Your MailChimp API key
       const listId = 'a63d2e77c7'; // Your MailChimp List ID
@@ -48,10 +28,10 @@ export async function POST(req, res) {
       // Handle the response from Mailchimp
       console.log(mailchimpResponse.data);
 
-      return new NextResponse('Welcome email sent and user subscribed to Mailchimp', { status: 200 });
+      return new NextResponse('User subscribed to Mailchimp', { status: 200 });
     } catch (error) {
-      console.error('Error sending welcome email or subscribing to Mailchimp', error);
-      return new NextResponse('Error sending welcome email or subscribing to Mailchimp', { status: 500 });
+      console.error('Error subscribing to Mailchimp', error);
+      return new NextResponse('Error subscribing to Mailchimp', { status: 500 });
     }
   } else {
     console.log('Use POST');
