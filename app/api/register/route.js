c213408f2374419840af8c7b3374a8b28293f819
Tahirkhan3013 <77354419+Tahirkhan3013@users.noreply.github.com>
2023-10-27 23:02:06 +0500

    Update route.js

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 export async function POST(req, res) {
   if (req.method === 'POST') {
     const body = await req.json();
-    const { email, firstName, lastName } = body; // Assuming you have collected email, firstName, and lastName
+    const { email } = body;
+    const { FNAME: firstName, LNAME: lastName } = body.merge_fields; // Collect firstName and lastName
 
     try {
       // Access environment variable directly
@@ -17,8 +18,8 @@ export async function POST(req, res) {
           email_address: email,
           status: 'subscribed',
           "merge_fields": {
-            "FNAME": "Test2",
-            "LNAME": lastName,
+            "FNAME": firstName, // Use the collected firstName
+            "LNAME": lastName,   // Use the collected lastName
           },
         },
         {
