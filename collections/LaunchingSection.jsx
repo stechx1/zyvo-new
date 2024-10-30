88bd38d451330dd0440e056fde50db964c9e6368
techx1 <109359174+stechx1@users.noreply.github.com>
2024-10-31 00:07:11 +0500

    Update LaunchingSection.jsx

--- collections/LaunchingSection.jsx
+++ collections/LaunchingSection.jsx
@@ -61,7 +61,7 @@ export const LaunchingSection = () => {
                 Launching
               </h2>
               <h2 className='md:text-5xl text-2xl text-[#3A4B4C] font-bold '>
-                August 2024
+                February 2025
               </h2>
             </div>
             <div
